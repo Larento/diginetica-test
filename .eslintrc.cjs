a4a22d02e4f93471274eff1271a69df73b9d2df6
Иларион Ерченко <turolretar@gmail.com>
2023-09-18 22:12:06 +0300

    add vite vue-ts template

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    ignorePatterns: ['dist'],
+    parser: '@typescript-eslint/parser',
+    overrides: [
+        {
+            files: ['**/*.cjs', '*.config.js'],
+            env: {
+                node: true,
+            },
+        },
+    ],
+};
